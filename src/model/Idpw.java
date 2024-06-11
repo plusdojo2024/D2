f2a85db137bf818cd9e6cabe3bce57e2d9925d37
daimon-ryota <daimon-ryota-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-11 16:52:31 +0900

    dao,model,servlet

--- src/model/Idpw.java
+++ src/model/Idpw.java
@@ -1,5 +1,46 @@
 package model;
 
-public class Idpw {
+import java.io.Serializable;
+
+public class Idpw implements Serializable {
+	private String id; // ID
+	private String pw; // PW
+	private String pc; // PC
+
+	public Idpw(String id, String pw, String pc) {
+		this.id = id;
+		this.pw = pw;
+		this.pc = pc;
+	}
+
+	public Idpw() {
+		this.id = "";
+		this.pw = "";
+		this.pc = "";
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getPw() {
+		return pw;
+	}
+
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+
+	public String getPc() {
+		return pc;
+	}
+
+	public void setPc(String pc) {
+		this.pc = pc;
+	}
 
 }
