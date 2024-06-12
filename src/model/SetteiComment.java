6155e4e8504781aee40385fe8b870a018b928ab1
plusdojo2024-tanaka-t <tanaka-tomoki-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-12 16:26:26 +0900

    comment

--- src/model/SetteiComment.java
+++ src/model/SetteiComment.java
@@ -1,5 +1,30 @@
 package model;
 
-public class SetteiComment {
+import java.io.Serializable;
+
+public class SetteiComment implements Serializable{
+	private String Date;//日付
+	private String ChildId;//子どものID
+	private String Comment ;// コメント
+
+	public String getDate() {
+		return Date;
+	}
+	public void setDate(String date) {
+		Date = date;
+	}
+	public String getChildId() {
+		return ChildId;
+	}
+	public void setChildId(String childId) {
+		ChildId = childId;
+	}
+	public String getComment() {
+		return Comment;
+	}
+	public void setComment(String comment) {
+		Comment = comment;
+	}
+
 
 }
