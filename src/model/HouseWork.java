c3bab260b3bf044bb2f0a5ca3e6f6ef3bfb3b112
plusdojo2024-masahisa <masahisa-hayato-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-12 13:07:44 +0900

    頭文字修正

--- src/model/HouseWork.java
+++ src/model/HouseWork.java
@@ -2,119 +2,119 @@ package model;
 import java.io.Serializable;
 
 public class HouseWork implements Serializable {
-	private String HouseworkName;//家事の名前
-	private String HouseworkContents;//家事内容
-	private String HouseworkPoint;//お手伝いポイント
-	private String Icon;//未処理のアイコン
-	private String IonDone;//処理済みのアイコン
-	private String UserId;//ログインID
-	private String IconX;//移動したアイコンのX座標
-	private String IconY;	// 移動したアイコンのY座標
+	private String houseworkName;//家事の名前
+	private String houseworkContents;//家事内容
+	private String houseworkPoint;//お手伝いポイント
+	private String icon;//未処理のアイコン
+	private String ionDone;//処理済みのアイコン
+	private String userId;//ログインID
+	private String iconX;//移動したアイコンのX座標
+	private String iconY;	// 移動したアイコンのY座標
 
 
 
 	public HouseWork(String houseworkName, String houseworkContents, String houseworkPoint, String icon, String ionDone,
 			String userId, String iconX, String iconY) {
-		this.HouseworkName = houseworkName;
-		this.HouseworkContents = houseworkContents;
-		this.HouseworkPoint = houseworkPoint;
-		this.Icon = icon;
-		this.IonDone = ionDone;
-		this.UserId = userId;
-		this.IconX = iconX;
-		this.IconY = iconY;
+		this.houseworkName = houseworkName;
+		this.houseworkContents = houseworkContents;
+		this.houseworkPoint = houseworkPoint;
+		this.icon = icon;
+		this.ionDone = ionDone;
+		this.userId = userId;
+		this.iconX = iconX;
+		this.iconY = iconY;
 	}
 
 
 	public HouseWork() {
-		this.HouseworkName =  "";
-		this.HouseworkContents =  "";
-		this.HouseworkPoint =  "";
-		this.Icon =  "";
-		this.IonDone =  "";
-		this.UserId = "";
-		this.IconX =  "";
-		this.IconY =  "";
+		this.houseworkName =  "";
+		this.houseworkContents =  "";
+		this.houseworkPoint =  "";
+		this.icon =  "";
+		this.ionDone =  "";
+		this.userId = "";
+		this.iconX =  "";
+		this.iconY =  "";
 	}
 
 
 	public String getHouseworkName() {
-		return HouseworkName;
+		return houseworkName;
 	}
 
 
 	public void setHouseworkName(String houseworkName) {
-		this.HouseworkName = houseworkName;
+		this.houseworkName = houseworkName;
 	}
 
 
 	public String getHouseworkContents() {
-		return HouseworkContents;
+		return houseworkContents;
 	}
 
 
 	public void setHouseworkContents(String houseworkContents) {
-		this.HouseworkContents = houseworkContents;
+		this.houseworkContents = houseworkContents;
 	}
 
 
 	public String getHouseworkPoint() {
-		return HouseworkPoint;
+		return houseworkPoint;
 	}
 
 
 	public void setHouseworkPoint(String houseworkPoint) {
-		this.HouseworkPoint = houseworkPoint;
+		this.houseworkPoint = houseworkPoint;
 	}
 
 
 	public String getIcon() {
-		return Icon;
+		return icon;
 	}
 
 
 	public void setIcon(String icon) {
-		this.Icon = icon;
+		this.icon = icon;
 	}
 
 
 	public String getIonDone() {
-		return IonDone;
+		return ionDone;
 	}
 
 
 	public void setIonDone(String ionDone) {
-		this.IonDone = ionDone;
+		this.ionDone = ionDone;
 	}
 
 
 	public String getUserId() {
-		return UserId;
+		return userId;
 	}
 
 
 	public void setUserId(String userId) {
-		this.UserId = userId;
+		this.userId = userId;
 	}
 
 
 	public String getIconX() {
-		return IconX;
+		return iconX;
 	}
 
 
 	public void setIconX(String iconX) {
-		this.IconX = iconX;
+		this.iconX = iconX;
 	}
 
 
 	public String getIconY() {
-		return IconY;
+		return iconY;
 	}
 
 
 	public void setIconY(String iconY) {
-		this.IconY = iconY;
+		this.iconY = iconY;
 	}
 
 
