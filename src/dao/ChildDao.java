c10d351f58b758d7c20797846eccee17710cff9b
plusdojo2024-fujiiyuka <fujii-yuka-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-18 15:52:14 +0900

    Update ChildDao.java

--- src/dao/ChildDao.java
+++ src/dao/ChildDao.java
@@ -166,7 +166,7 @@ public class ChildDao{
 			conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/data/D2", "sa", "");
 
 			// SQL文を準備する
-			String sql = "UPDATE Bc SET childId=?, childPicture=?, userId=?,rewardUmu=?, rewardJouken=?, rewardtext=?";
+			String sql = "UPDATE Child SET childId=?, childPicture=?, userId=?,rewardUmu=?, rewardJouken=?, rewardtext=?";
 			PreparedStatement pStmt = conn.prepareStatement(sql);
 
 			// SQL文を完成させる
