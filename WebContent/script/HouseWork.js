c81f6bf87b559595de9f1db8706ccb7b74ef0cb9
plusdojo2024-tanaka-t <tanaka-tomoki-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-20 12:04:03 +0900

    HW

---
+++ WebContent/script/HouseWork.js
@@ -0,0 +1,12 @@
+function makeTransparent(imgElement) {
+    // 半透明
+    imgElement.style.opacity = 0.5;
+
+    // 確認ダイアログ
+    var result = confirm("おわりましたか？");
+    if (!result) {
+        // キャンセルを選択した場合は透明度を元に戻す
+        imgElement.style.opacity = 1.0;
+    }
+    // 「OK」の場合は何もしない（既に透明化済み）
+}
